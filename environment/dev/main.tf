e90326f66752ac09c07cb93a2272222889ca3ea2
amanze123 <89152754+amanze123@users.noreply.github.com>
2022-12-07 14:56:51 +0000

    Update main.tf

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -13,7 +13,7 @@ module "dev_security_group" {
 
 module "dev_key_pair" {
   source = "../../modules/key_pair"
-  key_path = *.auto.tfvars
+  key_path = var.key_path
 }
 
 module "dev_docker_server" {
